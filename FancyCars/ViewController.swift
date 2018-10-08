9b07bdc8df56ff2e07f360d2ba190aa400656711
Vitaliy <makvitalik@gmail.com>
2018-10-08 16:34:04 +0300

    Initial Commit

---
+++ FancyCars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FancyCars
+//
+//  Created by Vitaliy Maksymlyuk on 10/8/18.
+//  Copyright © 2018 Vitaliy Maksymlyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
