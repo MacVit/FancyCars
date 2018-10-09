e1fb22f93b6e5203046b3f366794cb10ff611b9c
Vitaliy <makvitalik@gmail.com>
2018-10-09 14:51:14 +0300

    added ItemCollectionViewCell for reusable Item in a row

---
+++ FancyCars/Controllers/ItemViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemViewCell.swift
+//  FancyCars
+//
+//  Created by Vitaliy Maksymlyuk on 10/9/18.
+//  Copyright © 2018 Vitaliy Maksymlyuk. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageOfTheCar: RoundedImageView!
+    @IBOutlet weak var companyLbl: UILabel!
+    @IBOutlet weak var modelLbl: UILabel!
+    @IBOutlet weak var colorLbl: UILabel!
+    @IBOutlet weak var yearLbl: UILabel!
+    
+    
+}
