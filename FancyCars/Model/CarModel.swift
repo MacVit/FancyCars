0e0d21a39fd006f309b802adae58a6ed5233dd08
Vitaliy <makvitalik@gmail.com>
2018-10-09 17:23:30 +0300

    Added Car description struct

---
+++ FancyCars/Model/CarModel.swift
@@ -0,0 +1,17 @@
+//
+//  CarModel.swift
+//  FancyCars
+//
+//  Created by Vitaliy Maksymlyuk on 10/9/18.
+//  Copyright © 2018 Vitaliy Maksymlyuk. All rights reserved.
+//
+
+import Foundation
+
+struct CarDescription {
+    var company: String
+    var model: String
+    var year: String
+    var color: String
+    var image: Data
+}
