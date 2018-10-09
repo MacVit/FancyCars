5ef026e2f788d8da100f174a249ee30ae60af2ad
Vitaliy <makvitalik@gmail.com>
2018-10-09 14:11:40 +0300

    Added a custom Class to make images rounded

---
+++ FancyCars/New Group/RoundedImageView.swift
@@ -0,0 +1,15 @@
+//
+//  RoundedImageView.swift
+//  FancyCars
+//
+//  Created by Vitaliy Maksymlyuk on 10/8/18.
+//  Copyright © 2018 Vitaliy Maksymlyuk. All rights reserved.
+//
+
+import UIKit
+
+class RoundedImageView: UIImageView {
+
+    
+
+}
