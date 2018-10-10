33279bb0c8c11fbdd1dd842f86ea6f92a22c612f
Vitaliy <makvitalik@gmail.com>
2018-10-10 17:23:00 +0300

    Added Protocols for implementation of collection view

--- FancyCars/Controllers/MainListVC.swift
+++ FancyCars/Controllers/MainListVC.swift
@@ -16,8 +16,38 @@ class MainListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionListView.delegate = self
+        collectionListView.dataSource = self
     }
 
 
+}
+
+extension MainListVC: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
+extension MainListVC: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+   
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "toDetail", for: indexPath) as! ItemViewCell
+        
+        return cell
+    }
+    
+    
 }
 
