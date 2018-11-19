ad2075409f846233b87b2b2a6a293595c79d689c
Vitaliy <makvitalik@gmail.com>
2018-11-19 16:21:09 +0200

    Added Custom Data Manager for reading plist file

--- FancyCars/Protocols/LocalDataManagerProtocol.swift
+++ FancyCars/Protocols/LocalDataManagerProtocol.swift
@@ -8,6 +8,25 @@
 
 import Foundation
 
-protocol LocalDataManager {
-    static func extractObjectFrom<Type: Decodable>(plist name: String) -> Type?
+protocol LocalDataManagerProtocol {
+    static func extractObjectFrom<PlistType: Decodable>(plist name: String, of type: PlistType.Type) -> PlistType?
+}
+
+struct LocalDataManager: LocalDataManagerProtocol {
+    static func extractObjectFrom<PlistType>(plist name: String, of type: PlistType.Type) -> PlistType? where PlistType : Decodable {
+        let url: URL = Bundle.main.url(forResource: name, withExtension: "plist")!
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            let result = try decoder.decode(type, from: data)
+            
+            return result
+            
+        } catch {
+            print(error.localizedDescription)
+            
+            return nil
+        }
+    }
 }
